846894fb1b743a420d0a7949caa3252122bb9392
slisa0717 <s.lisa0717@gmail.com>
2026-06-13 20:20:42 +0900

    回答フォームの作成

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -2,4 +2,11 @@ class ChecksController < ApplicationController
     def index
          @word = Word.random
     end
+
+    def answer_check
+        @word = Word.find(params[:word_id])
+        @user_answer = params[:japanese]
+        @correct_answer = @word.japanese
+        @is_correct = @user_answer == @correct_answer
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'words#index'
   get '/lisa', to: 'top#lisa'
   get '/checks/index', to: 'checks#index' ,as:'shiken'
+  post '/checks/answer_check', to: 'checks#answer_check'
   resources :words
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
