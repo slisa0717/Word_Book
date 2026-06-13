44bde67e266322bc753a3dcbce6c91a233ad2c62
slisa0717 <s.lisa0717@gmail.com>
2026-06-13 19:02:28 +0900

    ボタンからchecksに飛べるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'words#index'
   get '/lisa', to: 'top#lisa'
-  get '/checks/index', to: 'checks#index'
+  get '/checks/index', to: 'checks#index' ,as:'shiken'
   resources :words
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
